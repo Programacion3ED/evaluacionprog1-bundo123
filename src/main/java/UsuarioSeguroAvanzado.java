2912bc0d993a01cd6e9c72a9462d3fd7e63e7721
bundo123 <davidgalarzanaranjo@gmail.com>
2026-04-22 10:54:30 -0500

    David Galarza v2

--- src/main/java/UsuarioSeguroAvanzado.java
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -8,4 +8,17 @@ public class UsuarioSeguroAvanzado {
     private int maxIntentos;
     private boolean accesoExitoso;
 
-}
+
+public UsuarioSeguroAvanzado(String username, String password, int maxIntentos) {
+        this.username = username;
+        this.password = password;
+        this.intentosFallidos = 0;
+        this.bloqueado = false;
+        this.accesoExitoso = false;
+        
+        if (maxIntentos <= 0) {
+            this.maxIntentos = 3;
+        } else {
+            this.maxIntentos = maxIntentos;
+        }
+}}
