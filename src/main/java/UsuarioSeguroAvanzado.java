1f3ae130cbdd90554b555d0395a2ad338a2d7d3c
bundo123 <davidgalarzanaranjo@gmail.com>
2026-04-22 10:43:59 -0500

    David Galarza v1

--- src/main/java/UsuarioSeguroAvanzado.java
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -1,4 +1,11 @@
 public class UsuarioSeguroAvanzado {
-
+    
+    // Atributos privados
+    private String username;
+    private String password;
+    private int intentosFallidos;
+    private boolean bloqueado;
+    private int maxIntentos;
+    private boolean accesoExitoso;
 
 }
