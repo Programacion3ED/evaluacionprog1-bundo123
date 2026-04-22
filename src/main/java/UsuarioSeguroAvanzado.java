938f52172fb2de1b870deacb1d263e98cb7e8fba
bundo123 <davidgalarzanaranjo@gmail.com>
2026-04-22 11:25:41 -0500

    David Galarza v5

--- src/main/java/UsuarioSeguroAvanzado.java
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -1,72 +1,29 @@
 public class UsuarioSeguroAvanzado {
-
+    
+    // Atributos privados
     private String username;
     private String password;
-    private int maxIntentos;
     private int intentosFallidos;
     private boolean bloqueado;
+    private int maxIntentos;
     private boolean accesoExitoso;
 
+    // Constructor
     public UsuarioSeguroAvanzado(String username, String password, int maxIntentos) {
         this.username = username;
         this.password = password;
-        // Si maxIntentos es 0 o negativo, usar 3 por defecto
-        this.maxIntentos = (maxIntentos <= 0) ? 3 : maxIntentos;
         this.intentosFallidos = 0;
         this.bloqueado = false;
         this.accesoExitoso = false;
-    }
-
-    public boolean autenticar(String passwordIngresada) {
-        if (bloqueado) {
-            return false;
-        }
-        if (password.equals(passwordIngresada)) {
-            intentosFallidos = 0;
-            accesoExitoso = true;
-            return true;
+        
+        if (maxIntentos <= 0) {
+            this.maxIntentos = 3;
         } else {
-            intentosFallidos++;
-            if (intentosFallidos >= maxIntentos) {
-                bloqueado = true;
-            }
-            return false;
+            this.maxIntentos = maxIntentos;
         }
     }
 
-    public void reiniciarAcceso() {
-        bloqueado = false;
-        intentosFallidos = 0;
-    }
-
-    public boolean validarPasswordSegura(String pass) {
-        if (pass == null || pass.length() < 8) {
-            return false;
-        }
-        boolean tieneMayuscula = false;
-        boolean tieneNumero = false;
-        for (char c : pass.toCharArray()) {
-            if (Character.isUpperCase(c)) tieneMayuscula = true;
-            if (Character.isDigit(c)) tieneNumero = true;
-        }
-        return tieneMayuscula && tieneNumero;
-    }
-
-    public boolean cambiarPassword(String actual, String nueva) {
-        if (bloqueado) {
-            return false;
-        }
-        if (!password.equals(actual)) {
-            return false;
-        }
-        if (!validarPasswordSegura(nueva)) {
-            return false;
-        }
-        password = nueva;
-        return true;
-    }
-
-    // Getters
+    // Métodos accesorios (Getters)
     public String getUsername() {
         return username;
     }
@@ -85,5 +42,67 @@ public class UsuarioSeguroAvanzado {
 
     public boolean isAccesoExitoso() {
         return accesoExitoso;
+    }
+
+    // Métodos de negocio
+
+    public boolean autenticar(String passwordIngresada) {
+        if (this.bloqueado) {
+            return false;
+        }
+        
+        if (this.password.equals(passwordIngresada)) {
+            this.intentosFallidos = 0;
+            this.accesoExitoso = true;
+            return true;
+        } else {
+            this.intentosFallidos++;
+            if (this.intentosFallidos >= this.maxIntentos) {
+                this.bloqueado = true;
+            }
+            return false;
+        }
+    }
+
+    public void reiniciarAcceso() {
+        this.intentosFallidos = 0;
+        this.bloqueado = false;
+    }
+    
+    public boolean validarPasswordSegura(String nueva) {
+        if (nueva == null || nueva.length() < 8) {
+            return false;
+        }
+        
+        boolean tieneMayuscula = false;
+        boolean tieneNumero = false;
+        
+        for (char c : nueva.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                tieneMayuscula = true;
+            }
+            if (Character.isDigit(c)) {
+                tieneNumero = true;
+            }
+        }
+        
+        return tieneMayuscula && tieneNumero;
+    }
+
+    public boolean cambiarPassword(String actual, String nueva) {
+        if (this.bloqueado) {
+            return false;
+        }
+        
+        if (!this.password.equals(actual)) {
+            return false;
+        }
+        
+        if (!validarPasswordSegura(nueva)) {
+            return false;
+        }
+        
+        this.password = nueva;
+        return true;
     }
 }
